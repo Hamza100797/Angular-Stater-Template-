86faee0e064e628f3d098e86a028eae6b23276d1
Hamza100797 <hbk100797@outlook.com>
2022-07-14 13:10:46 +0500

    Angular-stater template

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,35 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AdminViewComponent } from './_layouts/admin-layout/admin-view/admin-view/admin-view.component';
+import { ClientViewComponent } from './_layouts/client-layout/client-view/client-view/client-view.component';
+import { NavbarComponent } from './_layouts/client-layout/components/navbar/navbar/navbar.component';
+import { FooterComponent } from './_layouts/client-layout/components/footer/footer/footer.component';
+import { AdminDashboardComponent } from './Modules/main-modules/admin-Modules/pages/admin-dashboard/admin-dashboard.component';
+import { HomeComponent } from './Modules/main-modules/client-Modules/pages/home/home.component';
+import { NotFoundComponent } from './_layouts/common-components/404/not-found/not-found.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+// import { NgModule } from '@angular/core';
+import { NgxsModule } from '@ngxs/store';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AdminViewComponent,
+    ClientViewComponent,
+    NavbarComponent,
+    FooterComponent,
+    AdminDashboardComponent,
+    HomeComponent,
+    NotFoundComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    NgxsModule.forRoot(),
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
